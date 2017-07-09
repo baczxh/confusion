a100f50e82e3992e3b6e5fb1663dd053d3840318
George <baczxh@gmail.com>
2017-07-09 17:05:23 +0930

    NPM Scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+            $("#myCarousel").carousel({interval:2000});
+            $("#carousel-button").click(function(){
+                 if($('#carousel-button').children('span').hasClass('fa-pause')){
+                     $("#myCarousel").carousel('pause');
+                     $('#carousel-button').children('span').removeClass('fa-pause');
+                     $('#carousel-button').children('span').addClass('fa-play');
+                 }
+                 else{
+                     $("#myCarousel").carousel('cycle');
+                     $('#carousel-button').children('span').removeClass('fa-play');
+                     $('#carousel-button').children('span').addClass('fa-pause');
+                 }
+                 
+            });
+            
+        });
